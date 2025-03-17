a34c62865155b5cb0fb318da4027107ef21aa5e5
utkutasglk <utku.tasguluk@gmail.com>
2025-03-17 16:33:37 +0300

    Users metods created.

--- main.go
+++ main.go
@@ -17,6 +17,16 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/api",welcome)
 
 	app.Post("/api/users", routes.CreateUser)
+
+	app.Get("/api/users",routes.GetUsers)
+
+	app.Get("/api/users/:id",routes.GetUser)
+		
+	app.Put("/api/users/:id",routes.UpdateUser)
+
+	app.Delete("/api/users/:id",routes.DeleteUser)
+
+
 }
 
 func main(){
--- routes/user.go
+++ routes/user.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"errors"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/utkutasglk/Rest-Api-in-Go/database"
 	"github.com/utkutasglk/Rest-Api-in-Go/models"
@@ -27,4 +29,106 @@ func CreateUser(c *fiber.Ctx) error{
 	responseUser := CreateResponseUser(user)
 
 	return c.Status(200).JSON(responseUser)
+}
+
+func GetUsers(c *fiber.Ctx) error{
+	users := []models.User{}
+
+	database.Database.Db.Find(&users)
+	responseUsers := []UserSerializer{}
+
+	for _, user := range users{
+		responseUser := CreateResponseUser(user)
+		responseUsers = append(responseUsers, responseUser) 
+		}
+		return c.Status(200).JSON(responseUsers)
+
+}
+
+func FindUser(id int, user *models.User) error{
+	database.Database.Db.Find(&user, "id = ?", id)
+
+	if user.ID == 0 {
+		return errors.New("user dont exist")
+	}
+	return nil
+}
+
+func GetUser(c *fiber.Ctx) error{
+
+	id, err := c.ParamsInt("id")
+
+	var user models.User
+
+	if err != nil{
+		return c.Status(400).JSON("Please ensure that :id is an integer")
+	}
+
+	if err := FindUser(id, & user); err != nil {
+		return c.Status(400).JSON(err.Error())
+
+	}
+
+	responseUser := CreateResponseUser(user)
+
+	return c.Status(200).JSON(responseUser)
+
+}
+
+func UpdateUser(c *fiber.Ctx) error{
+	id, err := c.ParamsInt("id")
+
+	var user models.User
+
+	if err != nil{
+		return c.Status(400).JSON("Please ensure that :id is an integer")
+	}
+
+	if err := FindUser(id, & user); err != nil {
+		return c.Status(400).JSON(err.Error())
+
+	} 
+
+	type UpdateUser struct {
+		FirstName string `json:"first_name"`
+		LastName string `json:"last_name"`
+
+	}
+
+	var updateData UpdateUser
+
+	if err := c.BodyParser(&updateData); err != nil {
+		return c.Status(500).JSON(err.Error())
+	}
+
+	user.FirstName = updateData.FirstName
+	user.LastName = updateData.LastName
+
+	database.Database.Db.Save(&user)
+
+	responseUser := CreateResponseUser(user)
+	return c.Status(200).JSON(responseUser)
+}
+
+func DeleteUser(c *fiber.Ctx) error {
+
+	id, err := c.ParamsInt("id")
+
+	var user models.User
+
+	if err != nil{
+		return c.Status(400).JSON("Please ensure that :id is an integer")
+	}
+
+	if err := FindUser(id, & user); err != nil {
+		return c.Status(400).JSON(err.Error())
+
+	} 
+
+	if err := database.Database.Db.Delete(&user).Error; err != nil {
+		return c.Status(404).JSON(err.Error())
+	}
+
+	return c.Status(200).SendString("Successfully Deleted User")
+
 }
